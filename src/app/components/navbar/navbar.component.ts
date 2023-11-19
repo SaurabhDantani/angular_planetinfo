eab3d2cdfd0ca564e71c17eac781e78c410b2e55
Saurabh Dantani <saurabhdantani09@gmail.com>
2023-11-19 21:21:06 +0530

    now nav bar is dynamic

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  normalMenuItems: any[] = [
+    { label: 'Home', link: '/' },
+    { label: 'About Us', link: '/about' },
+    { label: 'Contact', link: '/contact' },
+  ];
+
+  firstMegaMenuItems: any[] = [
+    {
+      title: 'Products',
+      firstSubItems: [
+        {
+          title: 'GPS Trackers',
+          icon: '../../../assets/truck.png',
+          url: '/fuel',
+          description: 'GPS tracking, trip & inventory management'
+        },
+      ],
+
+      secondSubItems: [
+        {
+          title: 'Fleet Management Solution',
+          icon: '../../../assets/truck.png',
+          url: '/fleet',
+          description: 'GPS tracking, trip & inventory management'
+        },
+      ],
+
+    },
+  ];
 }
